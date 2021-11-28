7007742ec3e35ba65fd50f335af776d68a045cea
vinnn <vmoutel@yahoo.fr>
2021-11-28 23:10:42 +0000

    added button to restart board and added prompt to specify number of squares per side

--- README.md
+++ README.md
@@ -10,13 +10,13 @@ try: float/clear,  inline-block,  flexbox,  CSS Grid
 
 ## (3) Setup a "hover" effect
 - grid div change color, leaving pixelated trail as a pen would
-- can change color: add a new class, or chaning the div's background color with js
+- can change color: add a new class, or changing the div's background color with js
 
 ## (4) Add a button
 - that will clear the current grid
 - and send a popup asking the number of squares per side for the new grid
 - new grid should be created in the same total space as before (e.g. 960px wide)
-- set the limit for the user input to a max of 100 which is reasonable
+- set the limit for the user input to a max of 100 squares per side which is reasonable
 - for example, should be able to enter 64 and have a brand new 64x64 grid popup without changing the total amout of pixels used
 
 # (5) Change to a completely random RGB value
--- script.js
+++ script.js
@@ -1,68 +1,85 @@
-
-// 
-// 01. Event listener for 'push button'
-// 02. Display the Player's selection, the Computer's selection and the result
-// 03. Incremenet number of wins up to 5 rounds and display final winner
-
-
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// Initialise the scores and round number
+// Event listener
+// for the button being clicked
+// in which case: call newSnake function
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+const btn = document.querySelector('button');
+console.log(btn);
 
-
-// const playerWinsDiv=document.querySelector('.user-score');
-// playerWinsDiv.textContent = number_of_player_win;
+btn.addEventListener("click", newSnake);
 
 
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// Press button event listener
+// New Snake function 
+// (ie restart with a clean board)
+// - ask the number of squares per side for the new board
+// - create and display the new board
+// - change the squares background color when mouse hovering over
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// const btns = Array.from(document.querySelectorAll('button'));
-// console.log(btns);
+function newSnake() {
 
-// btns.forEach(btn => btn.addEventListener("click", playRound));
+    const container = document.querySelector('.container-squares');
 
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Remove all squares in order to clear any snake
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    while (container.lastChild) {
+        container.removeChild(container.lastChild);
+    }
 
-const container = document.querySelector('.container-squares');
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Ask for the number of square per side
+    // - Calculate the size of each square (in pix)
+    // - Calculate the total number of squares to be 
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    let nb_squares_per_side = prompt("Please enter the number of squares per side", 16);
 
-let nb_squares = 16 * 16;
-const squares = [];
+    side_px = (640 / nb_squares_per_side);
+    console.log(side_px);
 
-for (let i = 0; i < nb_squares; i++) {
+    let nb_squares = nb_squares_per_side * nb_squares_per_side;
+    console.log(nb_squares);    
 
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.textContent = i.toString();
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Create each square as an HTML 'div' element
+    // of class 'square'
+    // add the square dimensions as 'min-width' and 'min-height' 
+    // and add it inside the container div 
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    for (let i = 0; i < nb_squares; i++) {
+    
+        const square = document.createElement('div');
+        square.classList.add('square');
 
-    // container.append(square);
-    container.appendChild(square);
+        square.style.cssText = "min-width: " + side_px.toString() + "px; min-height: " +  side_px.toString() + "px;";
+    
+        container.appendChild(square);
+    }
+
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Event listener 
+    // for mouse over any square
+    // change the background color of that square 
+    // 
+    // can make the changed color remain for only a limited amount of time
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    let square_touched = document.querySelectorAll(".square");
+
+    square_touched.forEach((squar) => {
+        // This handler will be executed only once when the cursor
+        // moves over a square
+        squar.addEventListener("mouseover", function( event ) {
+            // event.target.style.color = "orange";
+            event.target.style.background = "yellow";
+            console.log(event);
+
+            // reset the color after a short delay
+            // setTimeout(function() {
+            // event.target.style.color = "";
+            // }, 500);
+        }, false);
+    });
 
 }
-
-
-
-let test = document.querySelectorAll(".square");
-
-test.forEach((squar) => {
-    // This handler will be executed only once when the cursor
-    // moves over a square
-    squar.addEventListener("mouseover", function( event ) {
-        event.target.style.color = "orange";
-    
-        // reset the color after a short delay
-        setTimeout(function() {
-        event.target.style.color = "";
-        }, 500);
-    }, false);
-    })
-
-
-
-
-
-
-
-
-
 
